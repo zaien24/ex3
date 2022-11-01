21b2244d04b0e1dfff239fa451f9ddc7af066dfb
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-11-02 00:16:07 +0900

    p110 파라미터 방식의 처리

--- src/main/java/org/zerock/ex3/controller/SampleController.java
+++ src/main/java/org/zerock/ex3/controller/SampleController.java
@@ -83,9 +83,14 @@ public class SampleController {
         model.addAttribute("list", list);
     }
 
-    @GetMapping("/exLayout1")
-    public void exLayout1(){
+//    @GetMapping("/exLayout1")
+//    public void exLayout1(){
+//
+//        log.info("exLayout............");
+//    }
 
+    @GetMapping({"/exLayout1","/exLayout2"})
+    public void exLayout1(){
         log.info("exLayout............");
     }
 
